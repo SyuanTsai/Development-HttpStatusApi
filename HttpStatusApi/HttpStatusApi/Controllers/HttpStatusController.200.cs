36ad8321ce44631d07a1d8e2fe9d4a833e978f67
SyuanTsai <SyuantTai@Super-Royal.com>
2021-12-22 09:38:33 +0800

    Feat : init

---
+++ HttpStatusApi/HttpStatusApi/Controllers/HttpStatusController.200.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpStatusApi.Controllers
+{
+    public partial class HttpStatusController : ControllerBase
+    {
+    }
+}
